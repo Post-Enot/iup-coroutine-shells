616798f20f019c9bf8e2266097d24a309ad4769b
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2022-11-05 03:16:51 +0300

    Update namespaces

--- Coroutine Shells/Runtime/UniqueCoroutine.cs
+++ Coroutine Shells/Runtime/UniqueCoroutine.cs
@@ -2,40 +2,22 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-namespace CoroutineShells
+namespace IUP_Toolkits.CoroutineShells
 {
-    /// <summary>
-    /// Класс-оболочка, инкапсулирующая логику взаимодействия с корутинами без параметров.
-    /// </summary>
     public sealed class UniqueCoroutine
     {
-        /// <summary>
-        /// Создаёт объект-оболочку, готовую к использованию.
-        /// </summary>
-        /// <param name="performer">MonoBehaviour-объект, с помощью которого будет запускаться корутина.
-        /// Отключение или удаление этого объекта приведёт к остановке действия корутины.</param>
-        /// <param name="getRoutine">Делегат-метод корутины.</param>
         public UniqueCoroutine(MonoBehaviour performer, Func<IEnumerator> getRoutine)
         {
             Performer = performer;
             _getRoutine = getRoutine;
         }
 
-        /// <summary>
-        /// Запущена ли корутина в текущий момент.
-        /// </summary>
         public bool IsPerformed { get; private set; }
-        /// <summary>
-        /// Ссылка на объект, с помощью которого запускается корутина.
-        /// </summary>
         public readonly MonoBehaviour Performer;
 
         private readonly Func<IEnumerator> _getRoutine;
         private Coroutine _coroutine;
 
-        /// <summary>
-        /// Запускает корутину, если она до этого не была запущена. В противном случае операция игнорируется.
-        /// </summary>
         public void Start()
         {
             if (!IsPerformed)
@@ -44,10 +26,6 @@ namespace CoroutineShells
             }
         }
 
-        /// <summary>
-        /// Запускает корутину.
-        /// Если она до этого была запущена, то принудительно завершает её выполнение, после чего запускает вновь.
-        /// </summary>
         public void StartAnyway()
         {
             if (IsPerformed)
@@ -57,9 +35,6 @@ namespace CoroutineShells
             _coroutine = Performer.StartCoroutine(Routine());
         }
 
-        /// <summary>
-        /// Останавливает корутину, если она до этого была запущена. В противном случае операция игнорируется.
-        /// </summary>
         public void Stop()
         {
             if (IsPerformed)
